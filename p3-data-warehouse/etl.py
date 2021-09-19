2e5556b531ac6bdc0c7ce442856a4666bc9e47eb
isaak-coursework <anderson.isaakr@gmail.com>
2021-09-19 11:18:57 -0400

    Add command line args for running one part of etl

--- p3-data-warehouse/etl.py
+++ p3-data-warehouse/etl.py
@@ -1,4 +1,6 @@
+import argparse
 import configparser
+
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
@@ -15,18 +17,43 @@ def insert_tables(cur, conn):
         conn.commit()
 
 
-def main():
+def main(insert_only, copy_only):
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+    if insert_only and copy_only:
+        raise ValueError('To run COPY and INSERTs, run without flags!')
+    
+    if not insert_only:
+        load_staging_tables(cur, conn)
+
+    if not copy_only:
+        insert_tables(cur, conn)
 
     conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load Sparkify data to Redshift!"
+    )
+    parser.add_argument(
+        '-i', '--insert',
+        action='store_true',
+        dest="insert_only",
+        default=False,
+        help='Only run INSERT statements to extract from staging tables. '
+    )
+    parser.add_argument(
+        '-c','--copy',
+        action='store_true',
+        dest="copy_only",
+        default=False,
+        help='Only run COPY statements to load data from S3'
+    )
+    args = parser.parse_args()
+    
+    main(args.insert_only, args.copy_only)
