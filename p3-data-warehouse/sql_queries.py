55c742ce0adfcd2efdb890d191408618da5fffda
isaak-coursework <anderson.isaakr@gmail.com>
2021-09-19 11:40:25 -0400

    Reformatting for convenience

--- p3-data-warehouse/sql_queries.py
+++ p3-data-warehouse/sql_queries.py
@@ -213,10 +213,10 @@ user_table_insert = """
 
 song_table_insert = """
     INSERT INTO songs (
-        song_id
-        title
-        artist_id
-        year
+        song_id,
+        title,
+        artist_id,
+        year,
         duration
     )
     SELECT 
@@ -240,7 +240,7 @@ artist_table_insert = """
         artist_id,
         artist_name,
         artist_location,
-        artist_lattitude,
+        artist_latitude,
         artist_longitude
     FROM staging_songs
 """
@@ -269,7 +269,35 @@ time_table_insert = """
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+create_table_queries = [
+    staging_events_table_create, 
+    staging_songs_table_create, 
+    time_table_create,
+    user_table_create, 
+    artist_table_create, 
+    song_table_create, 
+    songplay_table_create, 
+]
+
+drop_table_queries = [
+    staging_events_table_drop, 
+    staging_songs_table_drop, 
+    songplay_table_drop, 
+    user_table_drop, 
+    song_table_drop, 
+    artist_table_drop, 
+    time_table_drop
+]
+
+copy_table_queries = [
+    staging_events_copy, 
+    staging_songs_copy
+]
+
+insert_table_queries = [
+    songplay_table_insert, 
+    user_table_insert, 
+    song_table_insert, 
+    artist_table_insert, 
+    time_table_insert
+]
