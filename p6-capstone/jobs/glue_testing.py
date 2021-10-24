9fde4572b5f50e3f296123cadafe341b6959b92b
isaak-coursework <anderson.isaakr@gmail.com>
2021-10-24 11:29:39 -0400

    Modified to test fetching table

--- p6-capstone/jobs/glue_testing.py
+++ p6-capstone/jobs/glue_testing.py
@@ -1,14 +1,9 @@
-from awsglue.transforms import *
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 
-df = glueContext.create_dynamic_frame_from_catalog(
-    database="athenacurcfn_cur_monthly",
-    table_name="monthly"
-)
+df = glueContext.read.csv('s3a://p6-capstone/data/airport-codes_csv.csv')
 
 df.printSchema()
