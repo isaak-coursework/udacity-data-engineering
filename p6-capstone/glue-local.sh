4c01ddf0f6318dd3b1800f041183938a7e35ca58
isaak-coursework <anderson.isaakr@gmail.com>
2021-10-24 11:29:48 -0400

    Fixed submit option

--- p6-capstone/glue-local.sh
+++ p6-capstone/glue-local.sh
@@ -14,18 +14,20 @@ display_help() {
    echo "help             Print CLI help"
    echo "build            Build Image Locally"
    echo "pyspark          Start interactive Glue Pyspark shell with dependencies loaded!"
-   echo "glue-submit      Submit a script to Glue's Spark Submit Wrapper from 'jobs/' directory"
+   echo "submit           Submit a script to Glue's Spark Submit Wrapper from 'jobs/' directory"
    echo "bash             Open up a bash terminal inside container"
    echo
 }
 
 run_with_entrypoint() {
    # shellcheck disable=1091
-   source .env && docker run --rm -it \
+   source .env
+   docker run --rm -it \
       --volume "$PWD"/jobs:/root/aws-glue-libs/jobs \
       --env AWS_ACCESS_KEY_ID \
       --env AWS_SECRET_ACCESS_KEY \
       --env AWS_REGION="us-east-1" \
+      --env GLUE_JOB \
       --name $CONTAINER_NAME \
       --entrypoint "$1" \
       $IMAGE_TAG
@@ -36,13 +38,14 @@ build)
    docker build -t $IMAGE_TAG .
    ;;
 pyspark)
-    run_with_entrypoint "./bin/gluepyspark"
+   run_with_entrypoint "./bin/gluepyspark"
    ;;
 bash)
-    run_with_entrypoint "/bin/bash"
+   run_with_entrypoint "/bin/bash"
    ;;
-glue-submit)
-    run_with_entrypoint "./bin/gluesparksubmit ./jobs/$2"
+submit)
+   export GLUE_JOB="./jobs/$2"
+   run_with_entrypoint "./submit-job.sh"
    ;;
 help)
    display_help
