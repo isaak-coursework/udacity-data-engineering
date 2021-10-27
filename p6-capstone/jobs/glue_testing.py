5c257043b0eb48bd4ee44ed1d18e2e98d70724f3
isaak-coursework <anderson.isaakr@gmail.com>
2021-10-26 20:29:34 -0400

    Remove glue files - using pandas

--- p6-capstone/jobs/glue_testing.py
+++
@@ -1,9 +0,0 @@
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-
-sc = SparkContext()
-glueContext = GlueContext(sc)
-
-df = glueContext.read.csv('s3a://p6-capstone/data/airport-codes_csv.csv')
-
-df.printSchema()
--- p6-capstone/submit-job.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./bin/gluesparksubmit "$GLUE_JOB"
